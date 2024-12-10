d37a3c3d3eec055e5ca3cbd32784f01201d5ad25
JasminThari <s204155@student.dtu.dk>
2024-12-10 17:43:42 +0100

    Enhance exploratory analysis visualization and update power analysis effect size

--- src/explorative_analysis.py
+++ src/explorative_analysis.py
@@ -1,6 +1,3 @@
-
-
-
 #%%
 import pandas as pd
 import numpy as np
@@ -28,7 +25,10 @@ df_proportions['Experiment_Num'] = df_proportions['Experiment_Num'].astype('cate
 #%% Group by trail_num and calculate the mean proportion correct
 
 # Group by 'Experiment_Num' and 'trial_num' and calculate mean of 'Correct'
+grouped_trial = df.groupby(['trial_num'])['Correct'].mean().reset_index()
 grouped_df = df.groupby(['Experiment_Num', 'trial_num'])['Correct'].mean().reset_index()
+grouped_trial['Experiment_Num'] = 'All'
+grouped_df = pd.concat([grouped_df, grouped_trial], ignore_index=True)
 
 # Rename 'Correct' to 'Proportion_Correct' for clarity
 grouped_df.rename(columns={'Correct': 'Proportion_Correct'}, inplace=True)
@@ -37,64 +37,31 @@ grouped_df.rename(columns={'Correct': 'Proportion_Correct'}, inplace=True)
 sns.set(style="whitegrid")
 
 # Initialize the matplotlib figure
-plt.figure(figsize=(15, 6))
+plt.figure(figsize=(19, 7))
 
-# Create the line plot
+# Create the line plot with customized styles
 sns.lineplot(
     data=grouped_df,
     x='trial_num',
     y='Proportion_Correct',
     hue='Experiment_Num',
-    style='Experiment_Num',  # This ensures different line styles
-    markers=True,            # Optional: Adds markers to the lines
-    dashes=True,             # Use default dash patterns for different styles
-    linewidth=2, 
-    palette='ch:s=.25,rot=-.25'
-)
-
-# Customize the plot
-plt.title('Proportion of Correct Responses Across Trials by Group', fontsize=16)
-plt.xlabel('Trial Number', fontsize=14)
-plt.ylabel('Proportion Correct', fontsize=14)
-plt.legend(title='Group', fontsize=12, title_fontsize=13)
-plt.xticks(grouped_df['trial_num'].unique())  # Ensure all trial numbers are shown
-
-# Show the plot
-plt.tight_layout()
-plt.show()
-
-# %%
-grouped_df = df.groupby(['Pair_ID', 'Experiment_Num'])['Correct'].mean().reset_index()
-
-# Rename 'Correct' to 'Proportion_Correct' for clarity
-grouped_df.rename(columns={'Correct': 'Proportion_Correct'}, inplace=True)
-
-# Set the aesthetic style of the plots
-sns.set(style="whitegrid")
-
-# Initialize the matplotlib figure
-plt.figure(figsize=(25, 6))
-
-# Create the line plot
-sns.lineplot(
-    data=grouped_df,
-    x='Pair_ID',
-    y='Proportion_Correct',
-    hue='Experiment_Num',
-    style='Experiment_Num',  # This ensures different line styles
-    markers=True,            # Optional: Adds markers to the lines
-    dashes=True,             # Use default dash patterns for different styles
+    style='Experiment_Num',
+    palette={exp: 'black' if exp == 'All' else f'C{i}' for i, exp in enumerate(grouped_df['Experiment_Num'].unique())},
+    dashes={exp: '' if exp == 'All' else (2, 2) for exp in grouped_df['Experiment_Num'].unique()},
     linewidth=2
 )
 
 # Customize the plot
-plt.title('Proportion of Correct Responses Across Trials by Group', fontsize=16)
-plt.xlabel('Trial Number', fontsize=14)
-plt.ylabel('Proportion Correct', fontsize=14)
-plt.legend(title='Group', fontsize=12, title_fontsize=13)
-plt.xticks(grouped_df['Pair_ID'].unique())  # Ensure all trial numbers are shown
-
+plt.title('Proportion of Correct Responses Across Trials by Group', fontsize=23)
+plt.xlabel('Trial Number', fontsize=21)
+plt.ylabel('Proportion Correct', fontsize=21)
+plt.legend(title='Group', fontsize=16, title_fontsize=16)
+plt.xticks(grouped_df['trial_num'].unique(), fontsize=20, rotation=90)  # Adjust the fontsize and rotation as needed
+plt.yticks(fontsize=20)  # Adjust the fontsize as needed
 # Show the plot
 plt.tight_layout()
 plt.show()
+
+
+
 # %%
--- src/poweranalysis.py
+++ src/poweranalysis.py
@@ -6,10 +6,10 @@ from statsmodels.stats.power import FTestAnovaPower
 analysis = FTestAnovaPower()
 
 # Define parameters
-effect_size = 0.8  # Medium effect size (Cohen's f)
+effect_size = 0.2  # Medium effect size (Cohen's f)
 alpha = 0.05        # Significance level
-power = 0.8        # Desired power
-k_groups = 3        # Number of groups
+power = 0.80       # Desired power
+k_groups = 3       # Number of groups
 
 # Calculate the required sample size per group
 sample_size = analysis.solve_power(effect_size=effect_size, 
